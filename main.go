faf983cf0e579924893652a1e4fa97c332639414
Junior <eecfredes@gmail.com>
2026-05-18 22:37:40 -0300

    :rocket:Water cell

--- main.go
+++ main.go
@@ -8,7 +8,24 @@ const (
 	screenWidth  = 800
 	screenHeight = 450
 	title        = "💦 Water Simulation with Raylib-go"
+	gridWidth    = 160
+	gridHeight   = 90
+	cellHeight   = screenHeight / gridHeight //Size of each cell visually
+	cellWidth    = screenWidth / gridWidth
+	spreadFactor = 0.05                           // How quickly water spreads
+	dampening    = 0.999                          // Reduces wave energy over time
+	waterColor   = rl.NewColor(50, 100, 200, 255) // A nice blue
+	bedrockColor = rl.NewColor(80, 80, 80, 255)
 )
+
+type WaterCell struct {
+	height   float32
+	velocity float32
+}
+
+// Global grid for simplicity
+var waterGrid [gridWidth][gridHeight]WaterCell
+var newWaterGrid [gridWidth][gridHeight]WaterCell
 
 func main() {
 	rl.InitWindow(screenWidth, screenHeight, title)
